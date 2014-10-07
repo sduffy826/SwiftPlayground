97264037da45a95b6976ae255d9aef3d11bb9138
Sean Duffy <remi_96@hotmail.com>
2014-10-07 19:46:52 -0400

    Initial Commit

---
+++ SwiftPlayground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwiftPlayground
+//
+//  Created by Sean Regular on 10/7/14.
+//  Copyright (c) 2014 CS193p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwiftPlayground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwiftPlayground
+//
+//  Created by Sean Regular on 10/7/14.
+//  Copyright (c) 2014 CS193p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
